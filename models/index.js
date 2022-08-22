a480a9632be87f48d41a8d912af2615538eef995
Shuai Zuo <szuo1@vols.utk.edu>
2022-08-21 22:10:38 -0500

    finsih models folder

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Require users and thoughts models
+const Thoughts = require('./Thoughts');
+const Users = require('./Users');
+
+// Export modules: users and thoughts
+module.exports = { Users, Thoughts };
