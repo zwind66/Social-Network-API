6af46493e7731cda7ff889821d29c31620d6ee53
Shuai Zuo <szuo1@vols.utk.edu>
2022-08-21 22:30:41 -0500

    finsih routes

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// Set requirements for the app:
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).send('<h1>😝 404 Error!</h1>');
+});
+
+// Export router
+module.exports = router;
